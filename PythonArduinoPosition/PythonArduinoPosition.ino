849402fa78959d97780f1390df9db63b1f3f3bde
asdfire1 <45483619+asdfire1@users.noreply.github.com>
2019-10-30 11:33:58 +0100

    Update PythonArduinoPosition.ino

--- PythonArduinoPosition/PythonArduinoPosition.ino
+++ PythonArduinoPosition/PythonArduinoPosition.ino
@@ -13,7 +13,7 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 *******************************************************************************/
-//Antoni
+
 #include <Dynamixel2Arduino.h>
 
 
